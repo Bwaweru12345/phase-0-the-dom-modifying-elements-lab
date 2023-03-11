73025b14ff84758ae3121ef5825f7b024754e0de
Bwaweru12345 <bryanwaweru1076@example.com>
2023-03-11 21:25:39 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // let rem = document.getElementsByTagName("div")[0];
 // let rem2 = div.querySelector("main");
 // div.removeChild(rem2);
+// .
 
 main.remove();
 
 let newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "Richard is the champion";
+newHeader.innerHTML = "Richard is the champion"; 
